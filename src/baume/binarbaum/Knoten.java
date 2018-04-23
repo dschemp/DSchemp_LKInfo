fc6ef5d6e906125f68ea800cff75da910c44a4b4
Daniel Schemp <annomc1.yt@gmail.com>
2018-04-23 14:34:20 +0200

    Knoten added

--- src/baume/binarbaum/Knoten.java
+++ src/baume/binarbaum/Knoten.java
@@ -1,4 +1,18 @@
 package baume.binarbaum;
 
 public class Knoten {
+
+    public Knoten KnotenLinks;
+    public Knoten KnotenRechts;
+    public int KnotenInhalt;
+
+    public Knoten(int knotenInhalt) {
+        this.KnotenInhalt = knotenInhalt;
+    }
+
+    public Knoten(int knotenInhalt, Knoten knotenLinks, Knoten knotenRechts) {
+        this.KnotenInhalt = knotenInhalt;
+        this.KnotenLinks = knotenLinks;
+        this.KnotenRechts = knotenRechts;
+    }
 }
---
+++ src/baume/binarbaum/Knoten_Main.java
@@ -0,0 +1,9 @@
+package baume.binarbaum;
+
+public class Knoten_Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
