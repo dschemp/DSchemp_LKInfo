09d0ead825344338ad81043262cc95628e13e6fe
Daniel Schemp <annomc1.yt@gmail.com>
2018-04-26 09:34:52 +0200

    Liste sort()

--- src/liste/Liste.java
+++ src/liste/Liste.java
@@ -1,18 +1,29 @@
 package liste;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
+/**
+ * Liste mit doppelter Verknüpfung
+ */
 public class Liste {
     public ListElement head;
 
+    /**
+     * Leere Liste initialisieren
+     */
     public Liste() { }
 
-    public Liste(int[] array) {
+    /**
+     * Initalisiere eine Liste mit Inhalt aus einem Array
+     */
+    public Liste(int... array) {
         for (int x : array) {
             this.add(x);
         }
     }
 
+    /**
+     * Fügt ein Element am Ende der Liste ein
+     * @param content
+     */
     public void add(int content) {
         if (head == null)
             head = new ListElement(content, null, null);
@@ -21,6 +32,11 @@ public class Liste {
             lastElement.NextElement = new ListElement(content, null, lastElement);
         }
     }
+
+    /**
+     * Fügt ein Element an eine bestimmte Selle in der Liste ein
+     * @param index
+     */
     public void add(int content, int index) {
         ListElement indexElement = GetElement(index);
         ListElement lastIndexElement = indexElement.LastElement;
@@ -33,6 +49,10 @@ public class Liste {
         insertElement.NextElement = indexElement;
     }
 
+    /**
+     * Entfernt das Element an der Stelle 'index'
+     * @param index Index des zu entfernenden Elementes
+     */
     public void remove(int index) {
         if (index >= this.getSize())
             throw new IndexOutOfBoundsException();
@@ -50,11 +70,18 @@ public class Liste {
         nextDeleteElement.LastElement = lastDeleteElement;
     }
 
+    /**
+     * Entfernt das letzte Element der Liste
+     */
     public void remove() {
         ListElement lastElement = FindLastElement();
         lastElement.LastElement.NextElement = null;
     }
 
+    /**
+     * Ruft den Inhalt in der Liste an der Stelle index ab
+     * @return Inhalt an der Stelle
+     */
     public int getAtIndex(int index) {
         if (index >= this.getSize())
             throw new IndexOutOfBoundsException();
@@ -68,6 +95,9 @@ public class Liste {
         return end.Content;
     }
 
+    /**
+     * Tauscht den Inhalt zweier Elemente miteinander
+     */
     public void swap(int index_one, int index_two) {
         int listSize = this.getSize();
         if (index_one >= listSize || index_two >= listSize)
@@ -80,10 +110,23 @@ public class Liste {
         this.GetElement(index_two).Content = content_one;
     }
 
+    /**
+     * Sortiert die Liste per Insertionsort
+     */
     public void sort() {
-        throw new NotImplementedException();
+        for (int x = 1; x < this.getSize(); x++) {
+            for (int y = x; y > 0; y--) {
+                if (this.getAtIndex(y-1) > this.getAtIndex(y)) {
+                    this.swap(y, y-1);
+                }
+            }
+        }
     }
 
+    /**
+     * Abfrage nach der Anzahl der Elemente in der Liste
+     * @return Anzahl
+     */
     public int getSize() {
         ListElement end = head;
 
@@ -96,6 +139,10 @@ public class Liste {
         return length;
     }
 
+    /**
+     * Gibt die Liste als Array aus
+     * @return List als int Array
+     */
     public int[] toArray() {
         int[] array = new int[this.getSize()];
         for (int i = 0; i < array.length; i++)
--- src/liste/Liste_Main.java
+++ src/liste/Liste_Main.java
@@ -1,19 +1,23 @@
 package liste;
 
+import java.util.Random;
+
 public class Liste_Main {
 
     public static void main(String[] args) {
-        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-        Liste list = new Liste(array);
-        for (int i = 0; i < list.getSize(); i++) {
-            System.out.println(list.getAtIndex(i));
+        Random rnd = new Random();
+        Liste list2 = new Liste();
+        for (int i= 0; i < 10; i++) {
+            int num = rnd.nextInt(100);
+            list2.add(num);
+            System.out.println(num);
         }
-
-        System.out.println("---");
-
-        for (int x : list.toArray()) {
-            System.out.println(x);
+        System.out.println();
+        list2.sort();
+        list2.remove();
+        list2.remove();
+        for (int i = 0; i < list2.getSize(); i++) {
+            System.out.println(list2.getAtIndex(i));
         }
     }
 
