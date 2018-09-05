df695a7e38e9a040a8c92d1bf48f7b7a3630a12e
Daniel Schemp <annomc1.yt@gmail.com>
2018-09-05 22:29:36 +0200

    added methods to AES class

--- src/crypto/symmetric/AES.java
+++ src/crypto/symmetric/AES.java
@@ -1,4 +1,13 @@
 package crypto.symmetric;
 
 public class AES {
+
+    public byte[] Encrypt(byte[] data, int key) {
+        return null;
+    }
+
+    public byte[] Decrypt(byte[] encryptedData, int key) {
+        return null;
+    }
+
 }
