d8bf9b88a39cbccaf2f4c7b04232450c29d09d4e
Daniel Schemp <annomc1.yt@gmail.com>
2018-09-11 23:05:03 +0200

    added rsa using the build-in bigInteger type

---
+++ src/crypto/AsymmetricCrypto_Main.java
@@ -0,0 +1,21 @@
+package crypto;
+
+import crypto.asymmetric.RSA;
+
+import java.math.BigInteger;
+
+public class AsymmetricCrypto_Main {
+
+    public static void main(String[] args) {
+        BigInteger n = new BigInteger("3233");
+        BigInteger e = new BigInteger("17");
+        BigInteger d = new BigInteger("413");
+        byte[] data = { (byte)0x65 };
+
+        RSA rsa = new RSA();
+        byte[] cipherText = rsa.Encrypt(data, n, e);
+        byte[] plainText = rsa.Decrypt(cipherText, n, d);
+        System.out.println();
+    }
+
+}
--- src/crypto/asymmetric/RSA.java
+++ src/crypto/asymmetric/RSA.java
@@ -1,13 +1,17 @@
 package crypto.asymmetric;
 
+import java.math.BigInteger;
+
 public class RSA {
 
-    public byte[] Encrypt(byte[] data, int key) {
-        return null;
+    public byte[] Encrypt(byte[] data, BigInteger n, BigInteger e) {
+        BigInteger b = new BigInteger(data);
+        return b.modPow(e, n).toByteArray();
     }
 
-    public byte[] Decrypt(byte[] encryptedData, int key) {
-        return null;
+    public byte[] Decrypt(byte[] encryptedData, BigInteger n, BigInteger d) {
+        BigInteger b = new BigInteger(encryptedData);
+        return b.modPow(d, n).toByteArray();
     }
 
 }
