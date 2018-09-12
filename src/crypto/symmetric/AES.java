5873316f3c7ba4d087adb7fe9b209190091ed54e
Daniel Schemp <annomc1.yt@gmail.com>
2018-09-12 22:28:35 +0200

    added (inv)SubBytes with SBoxes

--- src/crypto/symmetric/AES.java
+++ src/crypto/symmetric/AES.java
@@ -25,16 +25,19 @@ public class AES {
     }
     //endregion
 
+    //region Key + Getter and Setter
     private byte[] Key;
 
-    //region Key Getter and Setter
-    public void setKey(byte[] key) {
+    private void setKey(byte[] key) {
         // Der Key darf nur aus 16- (128 bits), 24- (192 bits) oder 32- (256 bits) bytes bestehen
         // Zulaessig: 128, 192 oder 256 Bit
         if (key.length != 16 && key.length != 24 && key.length != 32) {
             throw new IllegalArgumentException("Key must be 128, 192 or 256 bits long");
         }
         this.Key = key;
+
+        byte[] expandedKey = ExpandKey(key);
+        setExpandedKey(expandedKey);
     }
 
     public byte[] getKey() {
@@ -42,12 +45,66 @@ public class AES {
     }
     //endregion
 
+    //region Expanded Key + Getter and Setter
+    private byte[] ExpandedKey;
+
+    private void setExpandedKey(byte[] expandedKey) {
+        this.ExpandedKey = expandedKey;
+    }
+
+    public byte[] getExpandedKey() {
+        return this.ExpandedKey;
+    }
+    //endregion
+
+    //region SBoxes
+    // SBox[x][y]
+    private byte[][] SBox = {
+            { (byte)0x63, (byte)0x7c, (byte)0x77, (byte)0x7b, (byte)0xf2, (byte)0x6b, (byte)0x6f, (byte)0xc5, (byte)0x30, (byte)0x01, (byte)0x67, (byte)0x2b, (byte)0xfe, (byte)0xd7, (byte)0xab, (byte)0x76, },
+            { (byte)0xca, (byte)0x82, (byte)0xc9, (byte)0x7d, (byte)0xfa, (byte)0x59, (byte)0x47, (byte)0xf0, (byte)0xad, (byte)0xd4, (byte)0xa2, (byte)0xaf, (byte)0x9c, (byte)0xa4, (byte)0x72, (byte)0xc0, },
+            { (byte)0xb7, (byte)0xfd, (byte)0x93, (byte)0x26, (byte)0x36, (byte)0x3f, (byte)0xf7, (byte)0xcc, (byte)0x34, (byte)0xa5, (byte)0xe5, (byte)0xf1, (byte)0x71, (byte)0xd8, (byte)0x31, (byte)0x15, },
+            { (byte)0x04, (byte)0xc7, (byte)0x23, (byte)0xc3, (byte)0x18, (byte)0x96, (byte)0x05, (byte)0x9a, (byte)0x07, (byte)0x12, (byte)0x80, (byte)0xe2, (byte)0xeb, (byte)0x27, (byte)0xb2, (byte)0x75, },
+            { (byte)0x09, (byte)0x83, (byte)0x2c, (byte)0x1a, (byte)0x1b, (byte)0x6e, (byte)0x5a, (byte)0xa0, (byte)0x52, (byte)0x3b, (byte)0xd6, (byte)0xb3, (byte)0x29, (byte)0xe3, (byte)0x2f, (byte)0x84, },
+            { (byte)0x53, (byte)0xd1, (byte)0x00, (byte)0xed, (byte)0x20, (byte)0xfc, (byte)0xb1, (byte)0x5b, (byte)0x6a, (byte)0xcb, (byte)0xbe, (byte)0x39, (byte)0x4a, (byte)0x4c, (byte)0x58, (byte)0xcf, },
+            { (byte)0xd0, (byte)0xef, (byte)0xaa, (byte)0xfb, (byte)0x43, (byte)0x4d, (byte)0x33, (byte)0x85, (byte)0x45, (byte)0xf9, (byte)0x02, (byte)0x7f, (byte)0x50, (byte)0x3c, (byte)0x9f, (byte)0xa8, },
+            { (byte)0x51, (byte)0xa3, (byte)0x40, (byte)0x8f, (byte)0x92, (byte)0x9d, (byte)0x38, (byte)0xf5, (byte)0xbc, (byte)0xb6, (byte)0xda, (byte)0x21, (byte)0x10, (byte)0xff, (byte)0xf3, (byte)0xd2, },
+            { (byte)0xcd, (byte)0x0c, (byte)0x13, (byte)0xec, (byte)0x5f, (byte)0x97, (byte)0x44, (byte)0x17, (byte)0xc4, (byte)0xa7, (byte)0x7e, (byte)0x3d, (byte)0x64, (byte)0x5d, (byte)0x19, (byte)0x73, },
+            { (byte)0x60, (byte)0x81, (byte)0x4f, (byte)0xdc, (byte)0x22, (byte)0x2a, (byte)0x90, (byte)0x88, (byte)0x46, (byte)0xee, (byte)0xb8, (byte)0x14, (byte)0xde, (byte)0x5e, (byte)0x0b, (byte)0xdb, },
+            { (byte)0xe0, (byte)0x32, (byte)0x3a, (byte)0x0a, (byte)0x49, (byte)0x06, (byte)0x24, (byte)0x5c, (byte)0xc2, (byte)0xd3, (byte)0xac, (byte)0x62, (byte)0x91, (byte)0x95, (byte)0xe4, (byte)0x79, },
+            { (byte)0xe7, (byte)0xc8, (byte)0x37, (byte)0x6d, (byte)0x8d, (byte)0xd5, (byte)0x4e, (byte)0xa9, (byte)0x6c, (byte)0x56, (byte)0xf4, (byte)0xea, (byte)0x65, (byte)0x7a, (byte)0xae, (byte)0x08, },
+            { (byte)0xba, (byte)0x78, (byte)0x25, (byte)0x2e, (byte)0x1c, (byte)0xa6, (byte)0xb4, (byte)0xc6, (byte)0xe8, (byte)0xdd, (byte)0x74, (byte)0x1f, (byte)0x4b, (byte)0xbd, (byte)0x8b, (byte)0x8a, },
+            { (byte)0x70, (byte)0x3e, (byte)0xb5, (byte)0x66, (byte)0x48, (byte)0x03, (byte)0xf6, (byte)0x0e, (byte)0x61, (byte)0x35, (byte)0x57, (byte)0xb9, (byte)0x86, (byte)0xc1, (byte)0x1d, (byte)0x9e, },
+            { (byte)0xe1, (byte)0xf8, (byte)0x98, (byte)0x11, (byte)0x69, (byte)0xd9, (byte)0x8e, (byte)0x94, (byte)0x9b, (byte)0x1e, (byte)0x87, (byte)0xe9, (byte)0xce, (byte)0x55, (byte)0x28, (byte)0xdf, },
+            { (byte)0x8c, (byte)0xa1, (byte)0x89, (byte)0x0d, (byte)0xbf, (byte)0xe6, (byte)0x42, (byte)0x68, (byte)0x41, (byte)0x99, (byte)0x2d, (byte)0x0f, (byte)0xb0, (byte)0x54, (byte)0xbb, (byte)0x16 }
+    };
+
+    // InvSBox[x][y]
+    private byte[][] InvSBox = {
+            { (byte)0x52, (byte)0x09, (byte)0x6a, (byte)0xd5, (byte)0x30, (byte)0x36, (byte)0xa5, (byte)0x38, (byte)0xbf, (byte)0x40, (byte)0xa3, (byte)0x9e, (byte)0x81, (byte)0xf3, (byte)0xd7, (byte)0xfb },
+            { (byte)0x7c, (byte)0xe3, (byte)0x39, (byte)0x82, (byte)0x9b, (byte)0x2f, (byte)0xff, (byte)0x87, (byte)0x34, (byte)0x8e, (byte)0x43, (byte)0x44, (byte)0xc4, (byte)0xde, (byte)0xe9, (byte)0xcb },
+            { (byte)0x54, (byte)0x7b, (byte)0x94, (byte)0x32, (byte)0xa6, (byte)0xc2, (byte)0x23, (byte)0x3d, (byte)0xee, (byte)0x4c, (byte)0x95, (byte)0x0b, (byte)0x42, (byte)0xfa, (byte)0xc3, (byte)0x4e },
+            { (byte)0x08, (byte)0x2e, (byte)0xa1, (byte)0x66, (byte)0x28, (byte)0xd9, (byte)0x24, (byte)0xb2, (byte)0x76, (byte)0x5b, (byte)0xa2, (byte)0x49, (byte)0x6d, (byte)0x8b, (byte)0xd1, (byte)0x25 },
+            { (byte)0x72, (byte)0xf8, (byte)0xf6, (byte)0x64, (byte)0x86, (byte)0x68, (byte)0x98, (byte)0x16, (byte)0xd4, (byte)0xa4, (byte)0x5c, (byte)0xcc, (byte)0x5d, (byte)0x65, (byte)0xb6, (byte)0x92 },
+            { (byte)0x6c, (byte)0x70, (byte)0x48, (byte)0x50, (byte)0xfd, (byte)0xed, (byte)0xb9, (byte)0xda, (byte)0x5e, (byte)0x15, (byte)0x46, (byte)0x57, (byte)0xa7, (byte)0x8d, (byte)0x9d, (byte)0x84 },
+            { (byte)0x90, (byte)0xd8, (byte)0xab, (byte)0x00, (byte)0x8c, (byte)0xbc, (byte)0xd3, (byte)0x0a, (byte)0xf7, (byte)0xe4, (byte)0x58, (byte)0x05, (byte)0xb8, (byte)0xb3, (byte)0x45, (byte)0x06 },
+            { (byte)0xd0, (byte)0x2c, (byte)0x1e, (byte)0x8f, (byte)0xca, (byte)0x3f, (byte)0x0f, (byte)0x02, (byte)0xc1, (byte)0xaf, (byte)0xbd, (byte)0x03, (byte)0x01, (byte)0x13, (byte)0x8a, (byte)0x6b },
+            { (byte)0x3a, (byte)0x91, (byte)0x11, (byte)0x41, (byte)0x4f, (byte)0x67, (byte)0xdc, (byte)0xea, (byte)0x97, (byte)0xf2, (byte)0xcf, (byte)0xce, (byte)0xf0, (byte)0xb4, (byte)0xe6, (byte)0x73 },
+            { (byte)0x96, (byte)0xac, (byte)0x74, (byte)0x22, (byte)0xe7, (byte)0xad, (byte)0x35, (byte)0x85, (byte)0xe2, (byte)0xf9, (byte)0x37, (byte)0xe8, (byte)0x1c, (byte)0x75, (byte)0xdf, (byte)0x6e },
+            { (byte)0x47, (byte)0xf1, (byte)0x1a, (byte)0x71, (byte)0x1d, (byte)0x29, (byte)0xc5, (byte)0x89, (byte)0x6f, (byte)0xb7, (byte)0x62, (byte)0x0e, (byte)0xaa, (byte)0x18, (byte)0xbe, (byte)0x1b },
+            { (byte)0xfc, (byte)0x56, (byte)0x3e, (byte)0x4b, (byte)0xc6, (byte)0xd2, (byte)0x79, (byte)0x20, (byte)0x9a, (byte)0xdb, (byte)0xc0, (byte)0xfe, (byte)0x78, (byte)0xcd, (byte)0x5a, (byte)0xf4 },
+            { (byte)0x1f, (byte)0xdd, (byte)0xa8, (byte)0x33, (byte)0x88, (byte)0x07, (byte)0xc7, (byte)0x31, (byte)0xb1, (byte)0x12, (byte)0x10, (byte)0x59, (byte)0x27, (byte)0x80, (byte)0xec, (byte)0x5f },
+            { (byte)0x60, (byte)0x51, (byte)0x7f, (byte)0xa9, (byte)0x19, (byte)0xb5, (byte)0x4a, (byte)0x0d, (byte)0x2d, (byte)0xe5, (byte)0x7a, (byte)0x9f, (byte)0x93, (byte)0xc9, (byte)0x9c, (byte)0xef },
+            { (byte)0xa0, (byte)0xe0, (byte)0x3b, (byte)0x4d, (byte)0xae, (byte)0x2a, (byte)0xf5, (byte)0xb0, (byte)0xc8, (byte)0xeb, (byte)0xbb, (byte)0x3c, (byte)0x83, (byte)0x53, (byte)0x99, (byte)0x61 },
+            { (byte)0x17, (byte)0x2b, (byte)0x04, (byte)0x7e, (byte)0xba, (byte)0x77, (byte)0xd6, (byte)0x26, (byte)0xe1, (byte)0x69, (byte)0x14, (byte)0x63, (byte)0x55, (byte)0x21, (byte)0x0c, (byte)0x7d }
+    };
+    //endregion
+
     public AES(byte[] key) {
         setKey(key);
     }
 
     //region Encrypt / Decrypt Methods
-    public byte[] Encrypt(byte[] data) {
+    private byte[] EncryptBlock(byte[] data) {
         // Anzahl der Runden bei AES
         // max(b, k) | 128 | 192 | 256
         // Runden r	 | 10  | 12  | 14
@@ -73,6 +130,7 @@ public class AES {
         // das Arrays ist folgendermaßen aufgebaut [Spalten][Reihen]
         byte[][] state = new byte[4][4];
 
+        // Befuelt das State Array aus dem Data Byte Array
         for (int i = 0; i < 16; i++) {
             int x = i / 4;
             int y = i % 4;
@@ -84,18 +142,39 @@ public class AES {
         return null;
     }
 
-    public byte[] Decrypt(byte[] encryptedData, byte[] key) {
+    private byte[] DecryptBlock(byte[] encryptedData) {
         return null;
     }
     //endregion
 
-    private byte[] AddRoundKey(byte[] state, byte[] key) {
+    //region Wrapper Methods
+    public byte[] Encrypt(byte[] data) {
+        // In Bloecke teilen, einzeln verschluesseln und dann zusammengefuegt returnen
         return null;
     }
 
+    public byte[] Decrypt(byte[] encryptedData) {
+        // In Bloecke teilen, einzeln entschluesseln und dann zusammengefuegt returnen
+        return null;
+    }
+    //endregion
+
+    //region AddRoundKey Method (for en- and decryption)
+    private byte[] AddRoundKey(byte[] state, byte[] expandedKey) {
+        return null;
+    }
+    //endregion
+
     //region Encryption Methods
     private byte[] SubBytes(byte[] state) {
-        return null;
+        for (int i = 0; i < state.length; i++) {
+            int firstNum  = (state[i] & 0xf0) >> 4;
+            int secondNum = (state[i] & 0xf);
+
+            state[i] = SBox[firstNum][secondNum];
+        }
+
+        return state;
     }
 
     private byte[] ShiftRows(byte[] state) {
@@ -109,7 +188,14 @@ public class AES {
 
     //region Decryption (Inverse) Methods
     private byte[] InvSubBytes(byte[] state) {
-        return null;
+        for (int i = 0; i < state.length; i++) {
+            int firstNum  = (state[i] & 0xF0) >> 4;
+            int secondNum = (state[i] & 0x0F);
+
+            state[i] = InvSBox[firstNum][secondNum];
+        }
+
+        return state;
     }
 
     private byte[] InvShiftRows(byte[] state) {
@@ -122,6 +208,10 @@ public class AES {
     //endregion
 
     //region Key Expansion
+    private byte[] ExpandKey(byte[] key) {
+        return null;
+    }
+
     private byte[] SubWord() {
         return null;
     }
